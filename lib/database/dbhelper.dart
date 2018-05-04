bd8dd689ce34d21e5dc985007c74230c5d795970
Raja Jawahar <rajajawahar77@gmail.com>
2018-05-04 15:40:07 +0530

    DBHelper added

---
+++ lib/database/dbhelper.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:io' as io;
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DBHelper{
+
+
+  // static final DBHelper instance = new DBHelper();
+
+  // factory DBHelper()=>instance;
+
+  static Database _db;
+
+  Future<Database> get db async {
+    if(_db != null)
+      return _db;
+    _db = await initDb();
+    return _db;
+  }
+
+  initDb() async {
+    io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "main.db");
+    var theDb = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return theDb;
+  }
+
+  void _onCreate(Database db, int version) async {
+    // When creating the db, create the table
+    await db.execute(
+    "CREATE TABLE User(id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
+    print("Created tables");
+  }
+
+
+}
